fb6cbde898d99b8b1d971bb9762199947709e742
richard phillips <richard.phillips@linn.co.uk>
2018-03-07 12:32:09 +0000

    made RetailerDemListRepository.cs

---
+++ src/Domain/Repositories/IRetailerDemListRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Linn.DemStock.Domain.Repositories
+{
+    public interface IRetailerDemListRepository
+    {
+        RetailerDemList GetById(int id);
+
+        RetailerDemList GetByRetailerUri(string retailerUri);
+
+        void Add(RetailerDemList retailerDemList);
+    }
+}
--- src/Facade/Services/DemStockService.cs
+++ src/Facade/Services/DemStockService.cs
@@ -2,9 +2,17 @@
 {
     using Linn.Common.Facade;
     using Linn.DemStock.Domain;
+    using Linn.DemStock.Domain.Repositories;
 
     public class DemStockService : IDemStockService
     {
+        private readonly IRetailerDemListRepository retailerDemListRepository;
+
+        public DemStockService(IRetailerDemListRepository retailerDemListRepository)
+        {
+            this.retailerDemListRepository = retailerDemListRepository;
+        }
+
         public IResult<RetailerDemList> GetRetailer(string retailerUri)
         {
             throw new System.NotImplementedException();
