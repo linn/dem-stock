a8b0317fdfa25fa5490032a5764645729150acb6
richard phillips <richard.phillips@linn.co.uk>
2018-03-07 10:52:02 +0000

    add ioc

--- src/IoC/ServiceModule.cs
+++ src/IoC/ServiceModule.cs
@@ -3,13 +3,22 @@
     using Amazon.SQS;
     using Autofac;
 
+    using Linn.Common.Facade;
     using Linn.Common.Logging;
     using Linn.Common.Logging.AmazonSqs;
+    using Linn.DemStock.Domain;
+    using Linn.DemStock.Facade;
+    using Linn.DemStock.Facade.ResourceBuilders;
 
     public class ServiceModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
+            // services
+            builder.RegisterType<DemStockService>().As<IDemStockService>();
+
+            // resource builders
+            builder.RegisterType<RetailerDemListResourceBuilder>().As<IResourceBuilder<RetailerDemList>>();
         }
     }
 }
