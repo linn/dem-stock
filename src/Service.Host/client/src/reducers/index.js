1a32394d9001f97dbf5b7d23055e57d40ba13ae0
Adam Campbell <Adam.Campbell@linn.co.uk>
2018-03-09 14:12:57 +0000

    added retailer dem list reducer

--- src/Service.Host/client/src/reducers/index.js
+++ src/Service.Host/client/src/reducers/index.js
@@ -1,10 +1,12 @@
 ﻿import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import retailerSearch from './retailerSearch';
+import retailerDemList from './retailerDemList';
 
 const rootReducer = combineReducers({
     router,
-    retailerSearch
+    retailerSearch,
+    retailerDemList
 });
 
 export default rootReducer;
---
+++ src/Service.Host/client/src/reducers/retailerDemList.js
@@ -0,0 +1,32 @@
+﻿import * as actionTypes from '../actions';
+
+const defaultState = {
+    retailerDemListUri: null,
+    loading: false,
+    item: null
+}
+
+const retailerDemList = (state = defaultState, action) => {
+    switch (action.type) {
+        case actionTypes.REQUEST_RETAILER_DEM_LIST:
+            return {
+                ...state,
+                item: null,
+                retailerDemListUri: action.payload.retailerDemListUri,
+                loading: true
+            }
+
+        case actionTypes.RECEIVE_RETAILER_DEM_LIST:
+            return {
+                ...state,
+                loading: false,
+                item: action.payload.data
+            }
+
+        default:
+            return state;
+    }
+
+}
+
+export default retailerDemList;
