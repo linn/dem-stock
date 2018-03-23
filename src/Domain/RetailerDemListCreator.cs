3574bfc9a7f477eb30f0b3685647bababab6fdd1
Adam Campbell <Adam.Campbell@linn.co.uk>
2018-03-23 16:22:43 +0000

    adding functionality to create a retailer dem list

---
+++ src/Domain/RetailerDemListCreator.cs
@@ -0,0 +1,45 @@
+﻿namespace Linn.DemStock.Domain
+{
+    using System;
+    using System.Data.SqlTypes;
+
+    using Linn.Common.Persistence;
+    using Linn.DemStock.Domain.Exceptions;
+    using Linn.DemStock.Domain.Repositories;
+    using Linn.DemStock.Domain.RetailerDemListActivities;
+
+    public class RetailerDemListCreator : ActivityEntity<RetailerDemListActivity>
+    {
+        private readonly ITransactionManager transactionManager;
+        private readonly IRetailerDemListRepository retailerDemListRepository;
+
+        public RetailerDemListCreator(ITransactionManager transactionManager, IRetailerDemListRepository retailerDemListRepository)
+        {
+            this.transactionManager = transactionManager;
+            this.retailerDemListRepository = retailerDemListRepository;
+        }
+
+        private RetailerDemListCreator()
+        {
+            // ef
+        }
+
+        public RetailerDemList CreateRetailerDemList(int retailerId, string createdBy)
+        {
+            if (this.retailerDemListRepository.GetByRetailerId(retailerId) != null)
+            {
+                throw new DuplicateRetailerDemListException($"Retailer {retailerId} already has a Dem List");
+            }
+
+            var retailerDemList = new RetailerDemList(retailerId);
+
+            this.Activities.Add(new CreateRetailerDemListActivity(createdBy));
+
+            this.retailerDemListRepository.Add(retailerDemList);
+
+            this.transactionManager.Commit();
+
+            return retailerDemList;
+        }
+    }
+}
---
+++ tests/Unit/Domain.Tests/RetailerDemListCreatorTests/ContextBase.cs
@@ -0,0 +1,26 @@
+﻿namespace Linn.DemStock.Domain.Tests.RetailerDemListCreatorTests
+{
+    using Linn.Common.Persistence;
+    using Linn.DemStock.Domain.Repositories;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public abstract class ContextBase
+    {
+        protected RetailerDemListCreator Sut { get; private set; }
+
+        protected ITransactionManager TransactionManager { get; private set; }
+
+        protected IRetailerDemListRepository DemListRepository { get; private set; }
+
+        [SetUp]
+        public void SetUpContext()
+        {
+            this.TransactionManager = Substitute.For<ITransactionManager>();
+            this.DemListRepository = Substitute.For<IRetailerDemListRepository>();
+            this.Sut = new RetailerDemListCreator(this.TransactionManager, this.DemListRepository);
+        }
+    }
+}
