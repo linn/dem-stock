ace04dab43ac9d52fda9609e516f4cc9638cdd96
atcampbell <adamtoddcampbell@gmail.com>
2018-04-23 11:10:14 +0100

    applying migrations (#36)

---
+++ src/Persistence/Migrations/20180423095155_create-activity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Linn.DemStock.Persistence.Migrations
+{
+    public partial class createactivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RetailerId",
+                table: "RetailerDemListActivity",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RetailerId",
+                table: "RetailerDemListActivity");
+        }
+    }
+}
