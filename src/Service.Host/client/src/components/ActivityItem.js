d9caf60564c3eea64191d263d4db9aab4e77c462
richard phillips <richard.phillips@linn.co.uk>
2018-04-24 14:03:29 +0100

    show created activities

--- src/Service.Host/client/src/components/ActivityItem.js
+++ src/Service.Host/client/src/components/ActivityItem.js
@@ -7,28 +7,36 @@ class ActivityItem extends Component {
     render() {
         const { activity, rootProductDetails } = this.props;
 
-        const isRootProductUpdate = (activity.activityType === 'UpdateRootProductActivity');
-        
-        return isRootProductUpdate 
-        ? (
-            <ListGroupItem>
-                <b>{getRootProductName(activity.rootProductUri, rootProductDetails)} </b>
-                quantity updated to <b>{activity.quantity} </b>
-                by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b>
-                <span className="small pull-right text-muted">{moment(activity.changedOn).startOf('hour').fromNow()}</span>
-            </ListGroupItem>
-        )
-        : (
-            <ListGroupItem>
-                <b>Last reviewed on </b>
-                {activity.lastReviewedOn 
-                    ? <span>updated to<b> {moment(activity.lastReviewedOn).format('DD MMM YYYY ')} </b>
-                        by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b></span>
-                    : <span>removed by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b></span>
-                }
-                <span className="small pull-right text-muted">{moment(activity.changedOn).startOf('hour').fromNow()}</span>
-            </ListGroupItem>
-        )
+        return activity.activityType === 'UpdateRootProductActivity'
+            ? (<ListGroupItem>
+                    <b>{getRootProductName(activity.rootProductUri, rootProductDetails)} </b>
+                    quantity updated to <b>{activity.quantity} </b>
+                    by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b>
+                    <span className="small pull-right text-muted">{moment(activity.changedOn).startOf('hour').fromNow()
+                    }</span>
+                </ListGroupItem>)
+            : activity.activityType === 'UpdateLastReviewedOnActivity'
+                ? (<ListGroupItem>
+                        <b>Last reviewed on </b>
+                        {activity.lastReviewedOn
+                            ? <span>updated to<b> {moment(activity.lastReviewedOn).format('DD MMM YYYY ')} </b>
+                                by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b></span>
+                            : <span>removed by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b></span>
+                        }
+                        <span className="small pull-right text-muted">{moment(activity.changedOn).startOf('hour').fromNow()
+                        }</span>
+                   </ListGroupItem>)
+                : activity.activityType === 'CreateRetailerDemListActivity'
+                    ? (<ListGroupItem>
+                        <b>Dem list created </b>
+                        <span>by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b></span>
+                        <span className="small pull-right text-muted">{moment(activity.changedOn).startOf('hour').fromNow()}</span>
+                        </ListGroupItem>)
+                    : (<ListGroupItem>
+                        <b>Unknown activity </b>
+                        <span>by <b>{activity.updatedBy ? activity.updatedBy : 'unknown user'}</b></span>
+                        <span className="small pull-right text-muted">{moment(activity.changedOn).startOf('hour').fromNow()}</span>
+                    </ListGroupItem>);
     }
 }
 
