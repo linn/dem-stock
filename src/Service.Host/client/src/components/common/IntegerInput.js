3d024ac9c0d9b1add918e186b18e6e148b0067f1
Adam Campbell <Adam.Campbell@linn.co.uk>
2018-05-15 16:24:46 +0100

    input mode for ios keyboard

--- src/Service.Host/client/src/components/common/IntegerInput.js
+++ src/Service.Host/client/src/components/common/IntegerInput.js
@@ -9,11 +9,12 @@ export class IntegerInput extends Component {
             <div ref={node => this.node = node}>
                 <input
                     style={{ maxWidth: '5em', marginRight: '20px' }}
-                    inputMode="numeric"
+                    type="text"
+                    pattern="\d*"
                     value={value}
                     onChange={e => this.handleChange(e.target.value)}
                     onKeyDown={e => onKeyDown(e)}
-                    ref={(FormControl) => this.numberInput = FormControl} />
+                    ref={(input) => this.numberInput = input} />
                 <Button bsClass="btn btn-xs btn-success muted" onClick={() => onClose()} disabled={okDisabled()}><Glyphicon glyph="saved" /></Button>
             </div>
         );
