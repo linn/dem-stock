755de12b5716e0295d906ef38a2640e111cb9589
richard phillips <richard.phillips@linn.co.uk>
2018-06-18 14:46:19 +0100

    root product selection

---
+++ src/Service.Host/client/src/containers/RetailerDemListsWithoutProductOptions.js
@@ -0,0 +1,18 @@
+﻿import { connect } from 'react-redux';
+import RetailerDemListsWithoutProductOptions from '../components/RetailerDemListsWithoutProductOptions';
+import { hideRootProductSearch, setRootProductSearchTerm } from '../actions/rootProductSearch';
+import { withRouter } from 'react-router'
+
+const mapStateToProps = ({ rootProductSearch }) => ({
+    rootProducts: rootProductSearch.items,
+    visible: rootProductSearch.visible,
+    loading: rootProductSearch.loading,
+    searchTerm: rootProductSearch.searchTerm
+});
+
+const mapDispatchToProps = {
+    hideRootProductSearch,
+    setRootProductSearchTerm
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(RetailerDemListsWithoutProductOptions));
